a2e133ae28755bf17a2faaa7723a95ae41521e85
Afani Martin <afanimartin@localhost.localdomain>
2019-10-14 12:51:36 +0300

    Add reverse string feature

--- functions.test.js
+++ functions.test.js
@@ -1,6 +1,11 @@
 const capitalize = require("./capitalize");
+const reverseString = require("./reverse")
 
 
 it("capitalize string", () => {
   expect(capitalize("test")).toEqual("Test");
+});
+
+it("reverse string", () => {
+  expect(reverseString("yes")).toBe("sey");
 });
